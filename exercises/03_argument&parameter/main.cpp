14793c305b90a0281a485d078fa4dde532bc1cd3
ych-yang <2844702574@qq.com>
2025-01-24 21:12:48 +0800

    完善ASSERT宏定义并修正输出信息中的拼写错误

--- exercises/03_argument&parameter/main.cpp
+++ exercises/03_argument&parameter/main.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
+#include <cstdlib>
 void func(int);
+using namespace std;
+#define ASSERT(cond, msg) if (!(cond)) { std::cerr << msg << std::endl; exit(1); }
 
-// TODO: 为下列 ASSERT 填写正确的值
 int main(int argc, char **argv) {
+// TODO: 为下列 ASSERT 填写正确的值
     auto arg = 99;
     ASSERT(arg == 99, "arg should be 99");
     std::cout << "befor func call: " << arg << std::endl;
@@ -14,7 +18,7 @@ int main(int argc, char **argv) {
 // TODO: 为下列 ASSERT 填写正确的值
 void func(int param) {
     ASSERT(param == 99, "param should be 99");
-    std::cout << "befor add: " << param << std::endl;
+    std::cout << "before add: " << param << std::endl;
     param += 1;
     ASSERT(param == 100, "param should be 100");
     std::cout << "after add: " << param << std::endl;
